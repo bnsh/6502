69c4569bd22434e28f606bf081d0c52ee8de7e05
Binesh Bannerjee <binesh_binesh@hotmail.com>
2025-03-08 15:10:02 -0500

    No need to store branch_tests.asm in the repo, if it's being generated.

--- cx16/debug/branch_tests.py
+++ cx16/debug/branch_tests.py
@@ -5,6 +5,14 @@
 
 def main():
     test = 0
+
+    branch_lessons = [
+        ("beq", "`beq` is Branch on Equal. (No surprises here.)"),
+        ("bne", "`bne` is Branch on Not Equal. (No surprises here.)"),
+        ("bmi", "`bmi` is Branch on Less Than. The Minus, is somewhat non intuitive to _me_."),
+        ("bpl", "`bpl` is Branch on Greater Than or Equal. The Plus, is also somewhat non intuitive to _me_."),
+    ]
+
     print("""
 .import debug_registers
 
@@ -26,26 +34,38 @@ def main():
     .byte $00, $00, $00             ; end of program
 
 """)
-    for branch in ("beq", "bne", "bpl", "bmi"):
+    for branch, lesson in branch_lessons:
+        print(f"; Tests for {branch:s}")
         for acc in (3, 4, 5):
-            print(f"""
-test{test:d}:
+            print(f"""test{test:d}:
     lda #${acc:02x}
     cmp #$04
     {branch:s} {branch:s}{acc:d}cmp4_success
     writestr_macro_nl {branch:s}{acc:d}cmp4_f
-    jmp test{test+1:d}
+    jmp test{test:d}_complete
 {branch:s}{acc:d}cmp4_success:
     writestr_macro_nl {branch:s}{acc:d}cmp4_s
-    jmp test{test+1:d}
+    jmp test{test:d}_complete
 {branch:s}{acc:d}cmp4_s:
     .asciiz "acc = {acc:d}, cmp 4, {branch:s} succeeds"
 {branch:s}{acc:d}cmp4_f:
     .asciiz "acc = {acc:d}, cmp 4, {branch:s} fails"
+test{test:d}_complete:
 """)
             test += 1
 
-    print(f"test{test:d}:\n    rts")
+        print(f"{branch:s}_lesson:")
+        print(f"    writestr_macro_nl {branch:s}_lesson_s")
+        print( "    lda #$0d")
+        print( "    jsr CHROUT")
+        print(f"    jmp {branch:s}_lesson_complete")
+        print(f"{branch:s}_lesson_s:")
+        print(f"    .asciiz \"{lesson:s}\"")
+        print(f"{branch:s}_lesson_complete:")
+
+        print()
+
+    print("    rts")
 
 if __name__ == "__main__":
     main()
